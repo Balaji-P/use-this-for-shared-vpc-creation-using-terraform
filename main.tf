b97895496881b72292c41423fb68ccf1145af39f
joel <joelcote68@gmail.com>
2020-01-25 11:25:49 -0600

    testing outputs

--- main.tf
+++ main.tf
@@ -12,5 +12,5 @@ data "terraform_remote_state" "afrl-bd-folder-id" {
 module "project" {
   source          = "./modules/shared_vpc_projects"
   billing_account_id = var.billing_account_id
-  folder_id       = data.terraform_remote_state.afrl-bd-folder-id.id
+  folder_id       = data.terraform_remote_state.afrl-bd-folder-id.outputs.afrl-bd-folder
 }
