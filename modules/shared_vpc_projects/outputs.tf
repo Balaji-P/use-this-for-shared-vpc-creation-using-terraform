dd1dec02c6299ed18506cd2ae590dda2dc4206b8
joel <joelcote68@gmail.com>
2020-01-25 12:02:57 -0600

    adding outputs

---
+++ modules/shared_vpc_projects/outputs.tf
@@ -0,0 +1,23 @@
+output "shared_vpc_host_project-id" {
+  value = google_project.host_project.id
+}
+
+output "afrl_big_data_project_id" {
+  value = google_project.service_project_1.id
+}
+
+output "afrl_app_engine_project_id" {
+  value = google_project.service_project_2.id
+}
+
+output "afrl_cloud_composer_project_id" {
+  value = google_project.service_project_3.id
+}
+
+output "shared_vpc_network_name" {
+  value = google_compute_network.shared_network.name
+}
+
+output "shared_vpc_subnetwork_1" {
+  value = google_compute_subnetwork.afrl-subnet-01.name
+}
---
+++ outputs.tf
@@ -0,0 +1,23 @@
+output "shared-vpc-name" {
+  value = module.project.shared_vpc_host_project-id
+}
+
+output "afrl-big-data-project-id" {
+  value = module.project.afrl_big_data_project_id
+}
+
+output "afrl-app-engine-project-id" {
+  value = module.project.afrl_app_engine_project_id
+}
+
+output "afrl-cloud-composer-project-id" {
+  value = module.project.afrl_cloud_composer_project_id
+}
+
+output "afrl-shared-vpc-network-name" {
+  value = module.project.shared_vpc_network_name
+}
+
+output "afrl-shared-vpc-subnet-1" {
+  value = module.project.shared_vpc_subnetwork_1
+}
