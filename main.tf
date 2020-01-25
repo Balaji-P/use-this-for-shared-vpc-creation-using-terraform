33d128e5b85b15391f4856546cd1cc2767cd8766
joel <joelcote68@gmail.com>
2020-01-25 11:23:03 -0600

    adding outputs

--- main.tf
+++ main.tf
@@ -1,5 +1,16 @@
+data "terraform_remote_state" "afrl-bd-folder-id" {
+  backend = "remote"
+  config = {
+    organization = "AFRLDigitalMFG"
+    workspaces = {
+      name = "folder"
+    }
+  }
+}
+
+
 module "project" {
   source          = "./modules/shared_vpc_projects"
   billing_account_id = var.billing_account_id
-  folder_id       = var.folder_id
+  folder_id       = data.terraform_remote_state.afrl-bd-folder-id.id
 }
